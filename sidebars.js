1493a7797caf6910993ad88cb69cf0d1a10c1e67
m0nch1 <m0nch1.develop@gmail.com>
2021-03-12 12:25:54 +0900

    update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ["doc1", "doc2", "doc3", "eslint", "prettier"],
+    Docusaurus: ["doc1", "doc2", "doc3", "doc4", "eslint", "prettier"],
     Features: ["mdx"],
   },
 };
